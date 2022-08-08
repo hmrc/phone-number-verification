028c75ad91a835a1e739aa245385a32362efeea1
Jonathan Stearnes <1640705+johno1985@users.noreply.github.com>
2022-08-08 14:08:24 +0100

    CAV-242: verify error message
     - added implicit converters for HttpReads
     - re-enabled IT test

--- app/uk/gov/hmrc/cipphonenumberverification/connectors/ValidateConnector.scala
+++ app/uk/gov/hmrc/cipphonenumberverification/connectors/ValidateConnector.scala
@@ -19,6 +19,7 @@ package uk.gov.hmrc.cipphonenumberverification.connectors
 import play.api.libs.json.Json
 import play.api.libs.ws.writeableOf_JsValue
 import uk.gov.hmrc.cipphonenumberverification.config.AppConfig
+import uk.gov.hmrc.http.HttpReads.Implicits._
 import uk.gov.hmrc.http.client.HttpClientV2
 import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse, StringContextOps}
 
