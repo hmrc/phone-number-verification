798c72843841e97def00c3e3f1669ff16d6fcaf8
brdgeth <brdgeth@users.noreply.github.com>
2022-09-07 16:00:06 +0100

    CAV-327 - Update hmrc libraries to the latest version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,16 +5,17 @@ import sbt._
 
 object AppDependencies {
 
-  val hmrcBootstrapVersion = "5.23.2-RC2"
+  val hmrcBootstrapVersion = "7.2.0"
+  val hmrcMongoPlayVersion = "0.71.0"
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % hmrcBootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.68.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % hmrcMongoPlayVersion,
     "io.jsonwebtoken"         %  "jjwt-api"                   % "0.10.8"
   )
 
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % hmrcBootstrapVersion  % "test, it",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.68.0"              % "test, it",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % hmrcMongoPlayVersion  % "test, it",
     "org.mockito"             %% "mockito-scala"              % "1.17.7"              % Test
   )
 }
