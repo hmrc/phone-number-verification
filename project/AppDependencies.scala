557982a31d62e451616bd69db098f0711fc37c7c
Saqib@Ee <saqib.saleem1@digital.hmrc.gov.uk>
2023-08-15 15:35:20 +0100

    CIR-1134: Bump deps versions & remove those that are not needed.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,20 +3,20 @@ import sbt.*
 
 object AppDependencies {
 
-  val hmrcBootstrapVersion = "7.2.0"
-  val hmrcMongoPlayVersion = "0.71.0"
+  val hmrcBootstrapVersion = "7.21.0"
+  val hmrcMongoPlayVersion = "1.3.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-backend-play-28"    % hmrcBootstrapVersion,
-    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"           % hmrcMongoPlayVersion,
-    "uk.gov.hmrc"                  %% "internal-auth-client-play-28" % "1.2.0",
-    "io.jsonwebtoken"               % "jjwt-api"                     % "0.11.5",
-    "com.googlecode.libphonenumber" % "libphonenumber"               % "8.13.12"
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % hmrcBootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % hmrcMongoPlayVersion,
+//    "uk.gov.hmrc"                  %% "internal-auth-client-play-28" % "1.2.0",
+    "io.jsonwebtoken"               % "jjwt-api"       % "0.11.5",
+    "com.googlecode.libphonenumber" % "libphonenumber" % "8.13.12"
   )
 
   val test = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-28"  % hmrcBootstrapVersion % "test, it",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28" % hmrcMongoPlayVersion % "test, it",
-    "org.mockito"       %% "mockito-scala"           % "1.17.12"            % "test, it"
+    "org.mockito"       %% "mockito-scala"           % "1.17.14"            % "test, it"
   )
 }
