ace4d6137bd33c0f2006bd4814a33efa0ae87f17
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2025-08-28 11:56:58 +0100

    [CIR-2310] Initial dependency bumps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapPlayVersion = "9.19.0"
+  private val bootstrapPlayVersion = "10.1.0"
   private val playSuffix           = "-play-30"
   private val mongoVersion         = "2.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% s"bootstrap-backend$playSuffix"    % bootstrapPlayVersion,
-    "uk.gov.hmrc"                  %% s"internal-auth-client$playSuffix" % "1.10.0",
+    "uk.gov.hmrc"                  %% s"internal-auth-client$playSuffix" % "4.1.0",
     "uk.gov.hmrc.mongo"            %% s"hmrc-mongo$playSuffix"           % mongoVersion,
     "com.googlecode.libphonenumber" % "libphonenumber"                   % "9.0.12"
   )
